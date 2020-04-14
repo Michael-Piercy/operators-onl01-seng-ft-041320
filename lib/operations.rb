86c511ada554153e53aed4bfa4dd39bfc47c7ace
Michael Piercy <michael.piercy@zoho.com>
2020-04-14 23:27:33 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
-  speed = 50
-  if speed == <60
-  elsif speed == >40
+  speed = '50'
+  if speed == '<60'
+  elsif speed == '>40'
 else
   return speed
 end
-end
+
 
 
 def not_safe?(speed)
