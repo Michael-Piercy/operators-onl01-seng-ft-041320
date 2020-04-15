0a5e7f15deb4942df215930271b20f86b0ec10f9
Michael Piercy <michael.piercy@zoho.com>
2020-04-15 01:17:20 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
 def unsafe?(speed)
-
+  if speed >60
+  return true
+  elsif speed <40
+  return true
+  else
+  return false
 end
-
+end
 
 
 def not_safe?(speed)
-	
+  speed >60 ? speed <40 : false
+	if speed >60
+	return true
+	elsif speed <40
+	return true
+	else
+	return false
 end
-	
+end	
 
 
